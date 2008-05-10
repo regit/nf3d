b38f2420e61bef348b2a7f0ac71c844baf6df05a
Eric Leblond <eric@bayen.regit.org>
2008-05-10 02:16:55 +0200

    Display a box under the connections.

--- conntrix.py
+++ conntrix.py
@@ -2,21 +2,25 @@ from visual import *
 from random import uniform, randint
 
 import pg
-
 import sys
+
+
+border = 0.2
+conn_count = 80
 
 def select_data():
   pgcnx = pg.connect('ulog2', 'localhost', 5432, None, None, 'ulog2', 'ulog2')
-  connlist = pgcnx.query("SELECT flow_start_sec, flow_end_sec FROM ulog2_ct  where flow_end_sec IS NOT NULL ORDER BY flow_start_sec ASC LIMIT 40").getresult()
+  connlist = pgcnx.query("SELECT flow_start_sec, flow_end_sec FROM ulog2_ct  where flow_end_sec IS NOT NULL ORDER BY flow_start_sec ASC LIMIT %s" % (conn_count)).getresult()
   return connlist
  
 connlist = select_data()
 
-border = 0.2
 scene.forward = (-0.25,-0.25,1)
+scene.autocenter = 1
+
 # Create some random boxes:
 nodes = []
-for t in range(40):
+for t in range(conn_count):
   b = cylinder( pos=(connlist[t][0]-connlist[0][0],0,2*t) )
   b.color = b.icolor = (0.5,0.5,1)
   height = connlist[t][1] - connlist[t][0] - border
@@ -34,10 +38,12 @@ for t in range(40):
 
 
 # Draw a grid:
-for i in range(21):
-    curve(pos=[(2*i-20,0,-20),(2*i-20,0,20)], color=color.cyan)       
-    curve(pos=[(-20,0,2*i-20),(20,0,2*i-20)], color=color.cyan)
-box(pos=(0,-0.6,0),width=40,length=40,height=1,color=(0,0,0.1))
+#for i in range(conn_count):
+#    curve(pos=[(2*i-20,0,-20),(2*i-20,0,20)], color=color.cyan)       
+#    curve(pos=[(-20,0,2*i-20),(20,0,2*i-20)], color=color.cyan)
+field_length = max(connlist)[1] - connlist[0][0] + 4
+field_width = conn_count*2 + 4
+box(pos=(field_length/2,-2,field_width/2),width=field_width,length=field_length,height=1,color=(0.5,0.5,0.5))
 
 # Drag and drop loop
 drag = None
