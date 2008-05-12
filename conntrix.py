4b0491598f8a96a5df1a46af452f7beb51c98f28
Eric Leblond <eric@inl.fr>
2008-05-12 15:30:40 +0200

    Display label when a connection is clicked.
    Display a label when a connection is clicked. Label contains dst IP, port,
    bytes out and in, duration.

--- conntrix.py
+++ conntrix.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python
+
 from visual import *
 from random import uniform, randint
 
@@ -6,11 +8,11 @@ import sys
 
 
 border = 0.2
-conn_count = 80
+conn_count = 40
 
 def select_data():
   pgcnx = pg.connect('ulog2', 'localhost', 5432, None, None, 'ulog2', 'ulog2')
-  connlist = pgcnx.query("SELECT flow_start_sec, flow_end_sec FROM ulog2_ct  where flow_end_sec IS NOT NULL ORDER BY flow_start_sec ASC LIMIT %s" % (conn_count)).getresult()
+  connlist = pgcnx.query("SELECT flow_start_sec+flow_start_usec/1000000 AS start, flow_end_sec+flow_end_sec/1000000 AS end, orig_ip_daddr_str, orig_l4_dport ,orig_raw_pktlen, reply_raw_pktlen FROM ulog2_ct  where flow_end_sec IS NOT NULL ORDER BY flow_start_sec ASC LIMIT %s" % (conn_count)).getresult()
   return connlist
  
 connlist = select_data()
@@ -19,21 +21,20 @@ scene.forward = (-0.25,-0.25,1)
 scene.autocenter = 1
 
 # Create some random boxes:
-nodes = []
+conns = []
 for t in range(conn_count):
-  b = cylinder( pos=(connlist[t][0]-connlist[0][0],0,2*t) )
-  b.color = b.icolor = (0.5,0.5,1)
+  conn = cylinder( pos=(connlist[t][0]-connlist[0][0],0,2*t) )
+  conn.color = conn.icolor = (0.5,0.5,1)
   height = connlist[t][1] - connlist[t][0] - border
-  b.axis = (height,0,0)
-  nodes.append( b )
+  conn.axis = (height,0,0)
+  conn.label = '%s:%d\n%d/%d bits\n%f sec' % (connlist[t][2], connlist[t][3], connlist[t][4], connlist[t][5],connlist[t][1] - connlist[t][0])
+  conns.append (conn)
   b = cylinder( pos=(connlist[t][0]-connlist[0][0],0,2*t) )
   b.color = b.icolor = (0.5,1,0.5)
   b.axis = (border,0,0)
-  nodes.append( b )
   b = cylinder( pos=(connlist[t][1]-connlist[0][0]-border,0,2*t) )
   b.color = b.icolor = (1,0.5,0.5)
   b.axis = (border,0,0)
-  nodes.append( b )
 
 
 
@@ -45,21 +46,17 @@ field_length = max(connlist)[1] - connlist[0][0] + 4
 field_width = conn_count*2 + 4
 box(pos=(field_length/2,-2,field_width/2),width=field_width,length=field_length,height=1,color=(0.5,0.5,0.5))
 
+prevlabel = None
+prevobject = None
 # Drag and drop loop
-drag = None
 while 1:
   if scene.mouse.events:
     c = scene.mouse.getevent()
-    if drag and (c.drop or c.click):   # drop the selected object
-      dp = c.project(normal=scene.up)
-      if dp: drag.pos = dp
-      drag.color = drag.icolor
-      drag = None
-    elif c.pick and hasattr(c.pick,"icolor"):   # pick up the object
-      drag = c.pick
-      drag.color = color.white
-  if drag:
-    dp = scene.mouse.project(normal=scene.up)
-    if dp: drag.pos = dp
-
-  #for lnk in links: lnk.pos = lnk.ends
+    if c.pick and hasattr(c.pick,"icolor"):   # pick up the object
+      if (hasattr(prevlabel, "pos")):
+	 prevlabel.visible = 0
+      if (hasattr(prevobject, "pos")):
+	 prevobject.color = prevobject.icolor = (0.5,0.5,1)
+      prevobject = c.pick
+      c.pick.color = (1,1,1)
+      prevlabel = label(pos=c.pick.pos, xoffset = 1, yoffset = 1, text = c.pick.label)
