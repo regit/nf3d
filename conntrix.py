4bdb30b5d2b761da233d38b494c1d6b0ec95f3f7
Eric Leblond <eric@inl.fr>
2008-05-13 23:47:14 +0200

    reindent.py is better than me (C and tab 4 ever)

--- conntrix.py
+++ conntrix.py
@@ -39,20 +39,20 @@ class connection(visual.cylinder):
 
     def __init__(self, start, end, **kargs):
         visual.cylinder.__init__(self, **kargs)
-	self.pos = (start,0, 0)
-	self.color = self.icolor = COLOR
-	self.radius = RADIUS
-	self.axis = (end - start,0,0)
-	self.label = "None"
-   
+        self.pos = (start,0, 0)
+        self.color = self.icolor = COLOR
+        self.radius = RADIUS
+        self.axis = (end - start,0,0)
+        self.label = "None"
+
     def ordonate(self, index):
         self.z = (3*RADIUS)*index
-	self.label.z = (3*RADIUS)*index
-    
+        self.label.z = (3*RADIUS)*index
+
     def set_label(self, ip_dest, port_dest, bytes_in, bytes_out):
         self.label = visual.label(pos=self.pos, xoffset = 10, yoffset = 10,  text='%s:%d\n%d/%d bits\n%f sec' % (ip_dest, port_dest, bytes_in, bytes_out, self.axis.x))
         self.label.visible = 0
-	self.set_port(port_dest)
+        self.set_port(port_dest)
 
     # FIXME: has to be improved
     def set_port(self, port):
@@ -66,23 +66,23 @@ class connections(list):
     def from_pgsql(self, count, **kargs):
         pgcnx = pg.connect('ulog2', 'localhost', 5432, None, None, 'ulog2', 'ulog2')
         conns = pgcnx.query("SELECT flow_start_sec+flow_start_usec/1000000 AS start, flow_end_sec+flow_end_sec/1000000 AS end, orig_ip_daddr_str, orig_l4_dport ,orig_raw_pktlen, reply_raw_pktlen FROM ulog2_ct  where flow_end_sec IS NOT NULL ORDER BY flow_start_sec ASC LIMIT %s" % (count)).getresult()
-	t = 0
-	self.count = count
-	for elt in conns:
-	    conn = connection(elt[0]-conns[0][0], elt[1]-conns[0][0])
-	    conn.set_label(elt[2], elt[3], elt[4], elt[5])
-	    conn.ordonate(t)
-	    self.append(conn)
-	    t += 1
+        t = 0
+        self.count = count
+        for elt in conns:
+            conn = connection(elt[0]-conns[0][0], elt[1]-conns[0][0])
+            conn.set_label(elt[2], elt[3], elt[4], elt[5])
+            conn.ordonate(t)
+            self.append(conn)
+            t += 1
 
     def length(self):
-	    return max(self).axis.x+max(self).pos.x
+        return max(self).axis.x+max(self).pos.x
 
     def plate(self):
-        field_length =  self.length() + 2*RADIUS 
+        field_length =  self.length() + 2*RADIUS
         field_width = 3*RADIUS*self.count + 10
         visual.box(pos = (field_length/2,-2,field_width/2), width = field_width, length = field_length, height = 1, color = BOX_COLOR)
- 
+
 visual.scene.forward = (0.25,0.25,10)
 visual.scene.autocenter = 1
 
@@ -98,13 +98,13 @@ while 1:
         c = visual.scene.mouse.getevent()
     if c.pick and hasattr(c.pick,"icolor"):   # pick up the object
         if not c.shift:
-	        for object in objlist:
-	            object.color = object.icolor = COLOR
-                object.label.visible = 0
+            for object in objlist:
+                object.color = object.icolor = COLOR
+            object.label.visible = 0
         objlist = []
     if (hasattr(c.pick, "label")):
         objlist.append(c.pick)
-	c.pick.label.visible = 1
+        c.pick.label.visible = 1
     c.pick.color = HIGHLIGHT_COLOR
     if visual.scene.kb.keys: # is there an event waiting to be processed?
         s = visual.scene.kb.getkey() # obtain keyboard information
@@ -112,7 +112,7 @@ while 1:
             if (s == 'p') and (len(objlist) == 1):
                 highlight = objlist[0]
                 for conn in connlist:
-		            if hasattr(conn, "dport") and  hasattr(highlight, "dport") and conn.dport == highlight.dport:
-		                objlist.append(conn)
-		                conn.color = HIGHLIGHT_COLOR
-		                conn.label.visible = 1
+                    if hasattr(conn, "dport") and  hasattr(highlight, "dport") and conn.dport == highlight.dport:
+                        objlist.append(conn)
+                        conn.color = HIGHLIGHT_COLOR
+                        conn.label.visible = 1
